c99d4a288738ce5660387f99a2ae204afab2a87a
ningyongheng <ningyongheng@jeejio.com>
2026-04-29 18:36:40 +0800

    feat: 配置日志打印

---
+++ src/components/logs/RequestLog.tsx
@@ -0,0 +1,37 @@
+// 生成一个tsx组件代码 
+// 第一步：样板代码使用文件引入
+// 这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+// （1）APIS TODO:
+
+// （2）Components
+
+// （3）Contexts
+
+// （4）Resources:图片图标资源，静态资源
+
+//   Styles：引入样式
+
+//   Types
+
+//   constants
+
+// 下面是逻辑组件部分
+const RequestLog = () => {
+  // （1）props:父组件传来的参数
+
+
+  // （6）Events
+
+  // （7）effect副作用函数
+
+  // （8）jsxComponent插件
+
+  return (
+    <><div>123</div></>
+  )
+
+};
+
+export {
+    RequestLog
+}
